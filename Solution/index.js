bc02e5f3a66968366d5907a7a11cc1caf62ea2aa
Jacob Paulin <jacobwpaulin@gmail.com>
2023-11-23 16:55:12 -0500

    feat: Challenge Completed

--- Solution/index.js
+++ Solution/index.js
@@ -1,12 +1,13 @@
 function thirdMostExpensive(obj) {
-    
+	const sortedItems = Object.entries(obj).sort((a, b) => b[1] - a[1]);
+    return sortedItems.length < 3 ? false : sortedItems[2][0];
 }
 
 // Solution Testing
-thirdMostExpensive({}) // Expected: false       Result: 
+console.log(thirdMostExpensive({})); // Expected: false 
 
-thirdMostExpensive({ piano: 100, tv: 200 }) // Expected: false      Result: 
+console.log(thirdMostExpensive({ piano: 100, tv: 200 })); // Expected: false
 
-thirdMostExpensive({ piano: 400, tv: 300, stereo: 200 })  // Expected: "stereo"     Result: 
+console.log(thirdMostExpensive({ piano: 400, tv: 300, stereo: 200 }));  // Expected: "stereo"
 
-thirdMostExpensive({ piano: 1000, tv: 500, ball: 10 , mirror: 200, }) // Expected: "mirror"     Result: 
+console.log(thirdMostExpensive({ piano: 1000, tv: 500, ball: 10 , mirror: 200, })); // Expected: "mirror"
